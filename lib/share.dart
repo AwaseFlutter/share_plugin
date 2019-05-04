642006959dd1a19e05d7ed92ee858bbe514537ff
neonankiti <yk.nanri@gmail.com>
2019-05-04 11:37:51 +0900

    create share interface in dart

--- lib/share.dart
+++ lib/share.dart
@@ -3,11 +3,10 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Share {
-  static const MethodChannel _channel =
-      const MethodChannel('share');
+  static const MethodChannel _channel = const MethodChannel('plugins/share');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<void> share(String text) {
+    assert(text != null || text.isNotEmpty);
+    return _channel.invokeMethod('share', text);
   }
 }
