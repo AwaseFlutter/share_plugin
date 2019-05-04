b71d2288c8bd6b7854532d7d8d221addf2904229
neonankiti <yk.nanri@gmail.com>
2019-05-04 12:58:36 +0900

    add activity context

--- android/src/main/kotlin/com/bison/share/SharePlugin.kt
+++ android/src/main/kotlin/com/bison/share/SharePlugin.kt
@@ -1,5 +1,6 @@
 package com.bison.share
 
+import android.app.Activity
 import android.content.Intent
 import android.text.TextUtils
 import io.flutter.plugin.common.MethodCall
@@ -8,24 +9,26 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class SharePlugin : MethodCallHandler {
+class SharePlugin(private val activity: Activity) : MethodCallHandler {
     companion object {
-        const val CHANNEL = "plugins/share"
+        private const val CHANNEL = "plugins/share"
 
         @JvmStatic
         fun registerWith(registrar: Registrar) {
+            val plugin = SharePlugin(registrar.activity())
             val channel = MethodChannel(registrar.messenger(), CHANNEL)
-            channel.setMethodCallHandler(SharePlugin())
+            channel.setMethodCallHandler(plugin)
         }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) = when (call.method) {
         "share" -> {
-            if (!TextUtils.isEmpty(call.argument<String>("text"))) {
-                share("")
+            val text = call.argument<String>("text")
+            if (!TextUtils.isEmpty(text)) {
+                share(text!!)
                 result.success(null)
             } else {
-                result.error("", "", "")
+                result.error("Error", "TextEmptyError", "Text is Null or Empty")
             }
         }
         else -> result.notImplemented()
@@ -41,5 +44,7 @@ class SharePlugin : MethodCallHandler {
             type = "text/plain"
         }
         val chooserIntent = Intent.createChooser(shareIntent, null)
+
+        activity.startActivity(chooserIntent)
     }
 }
